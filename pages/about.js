410dc66c4225e46eb0642dbf013debc336533885
its22013 <s22013@std-college.ac.jp>
2023-02-15 07:07:24 +0000

    4.5 Home, About, Blogのwidth調整

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function About () {
-  return <Hero />
+  return (
+    <Container>
+      <Hero />
+    </Container>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Container from 'components/container'
 import Hero from '../components/hero'
 
 export default function Home () {
   return (
-    <Hero
-      imageOn
-    />
+    <Container>
+      <Hero imageOn />
+    </Container>
   )
 }
