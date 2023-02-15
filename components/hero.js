a2e350fc07e7323dcfae814956b14c962484cb4c
its22013 <s22013@std-college.ac.jp>
2023-02-15 06:15:30 +0000

    4.3 hero layout

--- components/hero.js
+++ components/hero.js
@@ -2,7 +2,7 @@ import styles from 'styles/hero.module.css'
 
 export default function Hero ({ imageOn = false }) {
   return (
-    <div>
+    <div className={styles.flexContainer}>
       <div className={styles.text}>
         <h1 className={styles.title}>CUBE</h1>
         <p className={styles.subtitle}>アウトプットしていくサイト</p>
